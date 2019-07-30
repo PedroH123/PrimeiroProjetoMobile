2b569ed9740202cc49fae8695cb6b22a897908d0
PedroH123 <pedro_3ano@hotmail.com>
2019-07-31 03:05:31 +0900

    Funcionalidades Criadas

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,5 +1,10 @@
 // This is a JavaScript file
 
+$(document).on("click","#limpar",function(){
+
+  $("#resultado").val("");
+})
+
 $(document).on("click","#somar",function(){
 
   var resultado;
@@ -14,6 +19,78 @@ $(document).on("click","#subtrair",function(){
   var resultado;
 
   resultado = parseInt($("#valor1").val()) - parseInt($("#valor2").val());
+
+  $("#resultado").val(resultado);
+})
+
+$(document).on("click","#dividir",function(){
+
+  var resultado;
+
+  resultado = parseInt($("#valor1").val()) / parseInt($("#valor2").val());
+
+  $("#resultado").val(resultado);
+})
+
+$(document).on("click","#multiplicar",function(){
+
+  var resultado;
+
+  resultado = parseInt($("#valor1").val()) * parseInt($("#valor2").val());
+
+  $("#resultado").val(resultado);
+})
+
+$(document).on("click","#porcentagem",function(){
+
+  var resultado;
+
+  resultado = parseInt($("#valorX").val()) / 100;
+
+  $("#resultado").val(resultado);
+})
+
+$(document).on("click","#raiz",function(){
+
+  var resultado;
+  
+  var valor;
+
+  valor = parseInt($("#valorX").val());
+
+  resultado = Math.sqrt(valor);
+
+  $("#resultado").val(resultado);
+})
+
+$(document).on("click","#potenciacao",function(){
+
+  var resultado;
+  
+  var valor;
+  var valor2;
+
+
+  valor = parseInt($("#valorY").val());
+  valor2 = parseInt($("#valorZ").val());
+
+  resultado = Math.pow(valor,valor2);
+
+  $("#resultado").val(resultado);
+})
+
+$(document).on("click","#restoDivisao",function(){
+
+  var resultado;
+  
+  var valor;
+  var valor2;
+
+
+  valor = parseInt($("#valorY").val());
+  valor2 = parseInt($("#valorZ").val());
+
+  resultado = valor % valor2;
 
   $("#resultado").val(resultado);
 })
