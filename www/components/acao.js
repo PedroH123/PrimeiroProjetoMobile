ba462f95b25d99eac2b9fd09ba973226d60c7293
PedroH123 <pedro_3ano@hotmail.com>
2019-08-02 03:41:48 +0900

    Funcionalidades Concluidas

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,101 +1,172 @@
-// This is a JavaScript file
 
-$(document).on("click","#limpar",function(){
+var numero;
+var operacao;
+var resultado;
 
-  $("#resultado").val("");
-  $("#valor1").val("");
-    $("#valor2").val("");
-  $("#valorX").val("");
+$(document)
 
+$(document).on("click","#igual",function(){
+
+  var valorTotal2 = $("#resultado").val();
+  var valorTotal = valorTotal2.split(" ");
+
+  switch(valorTotal[1]){
+
+    case "+":
+       resultado = parseInt(valorTotal[0]) + parseInt(valorTotal[2]);
+       $("#resultado").val(resultado);
+    break;
+    case "-":
+       resultado = parseInt(valorTotal[0]) - parseInt(valorTotal[2]);
+       $("#resultado").val(resultado);
+    break;
+    case "X":
+       resultado = parseInt(valorTotal[0]) * parseInt(valorTotal[2]);
+       $("#resultado").val(resultado);
+    break;
+    case "^":
+       resultado = Math.pow(parseInt(valorTotal[0]),parseInt(valorTotal[2]));
+       $("#resultado").val(resultado);
+    break;
+    case "%":
+       resultado = parseInt(valorTotal[0]) / 100;
+       $("#resultado").val(resultado);
+    break;
+    case "√":
+       resultado = Math.sqrt(parseInt(valorTotal[0]));
+       $("#resultado").val(resultado);
+    break;
+    case "÷":
+       resultado = parseInt(valorTotal[0]) % parseInt(valorTotal[2]);
+       $("#resultado").val(resultado);
+    break;
+
+  }  
 })
 
+
+$(document).on("click","#1",function(){
+  numero = 1;
+  numero = $("#resultado").val() + numero;
+  $("#resultado").val(numero);
+})
+
+$(document).on("click","#2",function(){
+  numero = 2;
+  numero = $("#resultado").val() + numero;
+  $("#resultado").val(numero);
+})
+$(document).on("click","#3",function(){
+  numero = 3;
+  numero = $("#resultado").val() + numero;
+  $("#resultado").val(numero);
+})
+$(document).on("click","#4",function(){
+  numero = 4;
+  numero = $("#resultado").val() + numero;
+  $("#resultado").val(numero);
+})
+$(document).on("click","#5",function(){
+  numero = 5;
+  numero = $("#resultado").val() + numero;
+  $("#resultado").val(numero);
+})
+$(document).on("click","#6",function(){
+  numero = 6;
+  numero = $("#resultado").val() + numero;
+  $("#resultado").val(numero);
+})
+$(document).on("click","#7",function(){
+  numero = 7;
+  numero = $("#resultado").val() + numero;
+  $("#resultado").val(numero);
+})
+$(document).on("click","#8",function(){
+  numero = 8;
+  numero = $("#resultado").val() + numero;
+  $("#resultado").val(numero);
+})
+$(document).on("click","#9",function(){
+  numero = 9;
+  numero = $("#resultado").val() + numero;
+  $("#resultado").val(numero);
+})
+$(document).on("click","#0",function(){
+  numero = 0;
+  numero = $("#resultado").val() + numero;
+  $("#resultado").val(numero);
+})
+$(document).on("click","#ponto",function(){
+  numero = ".";
+  numero = $("#resultado").val() + numero;
+  $("#resultado").val(numero);
+})
+
+
+
 $(document).on("click","#somar",function(){
+operacao = " + ";
 
-  var resultado;
+operacao = $("#resultado").val() + operacao;
 
-  resultado = parseInt($("#valor1").val()) + parseInt($("#valor2").val());
-
-  $("#resultado").val(resultado);
+$("#resultado").val(operacao);
 })
 
 $(document).on("click","#subtrair",function(){
+operacao = " - ";
 
-  var resultado;
+operacao = $("#resultado").val() + operacao;
 
-  resultado = parseInt($("#valor1").val()) - parseInt($("#valor2").val());
-
-  $("#resultado").val(resultado);
+$("#resultado").val(operacao);
 })
 
 $(document).on("click","#dividir",function(){
+operacao = " / ";
 
-  var resultado;
+operacao = $("#resultado").val() + operacao;
 
-  resultado = parseInt($("#valor1").val()) / parseInt($("#valor2").val());
-
-  $("#resultado").val(resultado);
+$("#resultado").val(operacao);
 })
 
 $(document).on("click","#multiplicar",function(){
+operacao = " * ";
 
-  var resultado;
+operacao = $("#resultado").val() + operacao;
 
-  resultado = parseInt($("#valor1").val()) * parseInt($("#valor2").val());
-
-  $("#resultado").val(resultado);
-})
-
-$(document).on("click","#porcentagem",function(){
-
-  var resultado;
-
-  resultado = parseInt($("#valorX").val()) / 100;
-
-  $("#resultado").val(resultado);
-})
-
-$(document).on("click","#raiz",function(){
-
-  var resultado;
-  
-  var valor;
-
-  valor = parseInt($("#valorX").val());
-
-  resultado = Math.sqrt(valor);
-
-  $("#resultado").val(resultado);
+$("#resultado").val(operacao);
 })
 
 $(document).on("click","#potenciacao",function(){
+operacao = " ^ ";
 
-  var resultado;
-  
-  var valor;
-  var valor2;
+operacao = $("#resultado").val() + operacao;
 
+$("#resultado").val(operacao);
+})
 
-  valor = parseInt($("#valor1").val());
-  valor2 = parseInt($("#valor2").val());
+$(document).on("click","#porcentagem",function(){
+operacao = " % ";
 
-  resultado = Math.pow(valor,valor2);
+operacao = $("#resultado").val() + operacao;
 
-  $("#resultado").val(resultado);
+$("#resultado").val(operacao);
+})
+
+$(document).on("click","#raiz",function(){
+operacao = " √ ";
+
+operacao = $("#resultado").val() + operacao;
+
+$("#resultado").val(operacao);
 })
 
 $(document).on("click","#restoDivisao",function(){
+operacao = " ÷ ";
 
-  var resultado;
-  
-  var valor;
-  var valor2;
+operacao = $("#resultado").val() + operacao;
 
-
-  valor = parseInt($("#valor1").val());
-  valor2 = parseInt($("#valor2").val());
-
-  resultado = valor % valor2;
-
-  $("#resultado").val(resultado);
+$("#resultado").val(operacao);
 })
+
+
 
